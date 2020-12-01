1ccae8ab0bd1d457ef661c174350e9b1dec9ac60
naeem_hussien <naeem.hussien@bebit.com>
2020-12-01 23:56:00 +0900

    login

--- src/authenticate/views.py
+++ src/authenticate/views.py
@@ -1,4 +1,10 @@
+"""View url."""
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
   return render(request, 'authenticate/home.html', {})
+
+def login_user(request):
+  return render(request, 'authenticate/login.html', {})
