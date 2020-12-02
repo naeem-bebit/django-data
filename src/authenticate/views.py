2b95b04d0a28d567d1905f0ccc84d2d3b7ae0c2c
naeem_hussien <naeem.hussien@bebit.com>
2020-12-02 22:19:54 +0900

    redirect

--- src/authenticate/views.py
+++ src/authenticate/views.py
@@ -1,5 +1,5 @@
 """View url."""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 
@@ -7,4 +7,14 @@ def home(request):
   return render(request, 'authenticate/home.html', {})
 
 def login_user(request):
-  return render(request, 'authenticate/login.html', {})
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        return redirect('login')
+  else:
+    return render(request, 'authenticate/login.html', {})
